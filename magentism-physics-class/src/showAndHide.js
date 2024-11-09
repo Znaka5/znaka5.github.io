d43f77bdc0d5612576ffe98c6f6b7290cf82622a
Znaka5 <90087802+Znaka5@users.noreply.github.com>
2024-11-09 15:18:51 +0000

    physics.

---
+++ magentism-physics-class/src/showAndHide.js
@@ -0,0 +1,11 @@
+export function showScrolls(element) {
+    element.style.visibility = "visible"
+}
+
+export function showBtn(btn) {
+    document.getElementById(btn).style.display = "inline-block"
+}
+
+export function hideBtn(btn) {
+    document.getElementById(btn).style.display = "none"
+}
